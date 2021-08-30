aa05bf49f25fd9b67f88d1c67439e25ef163797a
subodh <subodh1810@gmail.com>
2021-08-30 16:53:41 +0530

    test out git revision command

--- bump_version.sh
+++ bump_version.sh
@@ -15,5 +15,5 @@ NEW_VERSION=$(grep VERSION version.txt | cut -d"=" -f2)
 GIT_REVISION=$(git rev-parse HEAD)
 [[ -z "$GIT_REVISION" ]] && echo "Couldn't get the git revision..." && exit 1
 
-echo "Bumped version from ${PREV_VERSION} to ${NEW_VERSION}"
+echo "Bumped version to ${NEW_VERSION}"
 echo "Building and publishing version $NEW_VERSION for git revision $GIT_REVISION..."

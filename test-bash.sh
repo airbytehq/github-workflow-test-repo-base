d54d49a985f815af1c254cf58921f1baa76e92d4
subodh <subodh1810@gmail.com>
2021-08-31 03:24:14 +0530

    fix script

--- test-bash.sh
+++ test-bash.sh
@@ -1,3 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
 newline="\n"
 changelog="Changelog:"
 commits=$(PAGER=cat git log 206793dfd3d863d484851f4b7fae0e87cd87a8f0..2295dd98eb55faf22736cbbbb0e5e6534c2b3a63 --oneline --decorate=no)
